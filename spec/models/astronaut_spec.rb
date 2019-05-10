53c553c195323e4194bf5536515c91089c25d0d4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-10 09:37:19 -0600

    Add model test for Astronaut::average_age

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,15 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'Class Methods' do
+    it "calculates average age of all astronauts" do
+      astronaut_1 = Astronaut.create!(name: "Buzz Aldrin", age: 40, job: "Space Guy")
+      astronaut_2 = Astronaut.create!(name: "Neil Armstrong", age: 37, job: "Commander")
+
+      avg_age = (@astronaut_1.age + @astronaut_2.age).to_f / 2
+
+      expect(Astronaut.average_age).to eq(avg_age)
+    end
+  end
 end
